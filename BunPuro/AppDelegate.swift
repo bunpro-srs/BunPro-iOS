5d24e3001142928d2552efdf05220547a17022c5
Andreas Braun <rion@canta-per-me.net>
2018-01-15 21:00:18 +0100

    Bugfix for the notification badge, should not be sound twice haha

--- BunPuro/AppDelegate.swift
+++ BunPuro/AppDelegate.swift
@@ -85,7 +85,7 @@ extension AppDelegate: UNUserNotificationCenterDelegate {
     
     func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
      
-        completionHandler([.sound, .sound, .alert])
+        completionHandler([.sound, .badge, .alert])
     }
     
     func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
