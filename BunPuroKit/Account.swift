cd62da75c7c448a161b508ad0cf6f87bca17cd31
Andreas Braun <rion@canta-per-me.net>
2018-10-15 17:45:36 +0200

    Made Settings Enums fail more gracefully

--- BunPuroKit/Account.swift
+++ BunPuroKit/Account.swift
@@ -3,21 +3,83 @@ import Foundation
 public enum Active: String, Codable {
     case yes = "Yes"
     case no = "No"
+    
+    public init(from decoder: Decoder) throws {
+        guard let value = try? decoder.singleValueContainer().decode(String.self) else {
+            self = .no
+            return
+        }
+        
+        guard let state = Active(rawValue: value) else {
+            
+            print("Invalid value for \(Active.self): \(value)")
+
+            switch value {
+            case "\"On\"", "Show", "\"Show\"", "\"Yes\"": self = .yes
+            default: self = .no
+            }
+            return
+        }
+        
+        self = state
+    }
 }
 
 public enum Visible: String, Codable {
     case show = "Show"
     case hide = "Hide"
     case minimal = "Minimal"
+    
+    public init(from decoder: Decoder) throws {
+        guard let value = try? decoder.singleValueContainer().decode(String.self) else {
+            self = .hide
+            return
+        }
+        
+        guard let state = Visible(rawValue: value) else {
+            
+            print("Invalid value for \(Visible.self): \(value)")
+            
+            switch value {
+            case "\"On\"", "Show", "\"Show\"": self = .show
+            case "\"Minimal\"": self = .minimal
+            default: self = .hide
+            }
+            return
+        }
+        
+        self = state
+    }
 }
 
 public enum State: String, Codable {
     case on = "On"
     case off = "Off"
+    
+    public init(from decoder: Decoder) throws {
+        guard let value = try? decoder.singleValueContainer().decode(String.self) else {
+            self = .off
+            return
+        }
+        
+        guard let state = State(rawValue: value) else {
+            
+            print("Invalid value for \(State.self): \(value)")
+            
+            switch value {
+            case "\"On\"", "Show", "\"Show\"": self = .on
+            default: self = .off
+            }
+            return
+        }
+        
+        
+        self = state
+    }
 }
 
 public enum FuriganaMode: String, Codable {
-    case on = "On"
+    case on = "Show"
     case off = "Off"
     case wanikani = "Wanikani"
     
@@ -36,6 +98,27 @@ public enum FuriganaMode: String, Codable {
         }
         
         self = mode
+    }
+    
+    public init(from decoder: Decoder) throws {
+        guard let value = try? decoder.singleValueContainer().decode(String.self) else {
+            self = .off
+            return
+        }
+        
+        guard let state = FuriganaMode(rawValue: value) else {
+            
+            print("Invalid value for \(FuriganaMode.self): \(value)")
+
+            switch value {
+            case "\"On\"", "\"Show\"", "Yes", "\"Yes\"": self = .on
+            case "Wanikani", "\"Wanikani\"": self = .wanikani
+            default: self = .off
+            }
+            return
+        }
+        
+        self = state
     }
 }
 
